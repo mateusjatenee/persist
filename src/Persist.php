4057a28096b5627cbbcd95807af022169cf4936e
Mateus Guimarães <mateus.jatene@gmail.com>
2023-11-22 15:41:37 -0300

    proxy call to parent setAttribute

--- src/Persist.php
+++ src/Persist.php
@@ -2,6 +2,7 @@
 
 namespace Mateusjatenee\Persist;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
@@ -90,59 +91,10 @@ trait Persist
      */
     public function setAttribute($key, $value)
     {
-        // First we will check for the presence of a mutator for the set operation
-        // which simply lets the developers tweak the attribute as it is set on
-        // this model, such as "json_encoding" a listing of data for storage.
-        if ($this->hasSetMutator($key)) {
-            return $this->setMutatedAttributeValue($key, $value);
-        } elseif ($this->hasAttributeSetMutator($key)) {
-            return $this->setAttributeMarkedMutatedAttributeValue($key, $value);
-        }
-
-        // If an attribute is listed as a "date", we'll convert it from a DateTime
-        // instance into a form proper for storage on the database tables using
-        // the connection grammar's date format. We will auto set the values.
-        elseif (! is_null($value) && $this->isDateAttribute($key)) {
-            $value = $this->fromDateTime($value);
-        }
-
-        if ($this->isEnumCastable($key)) {
-            $this->setEnumCastableAttribute($key, $value);
-
-            return $this;
-        }
-
-        if ($this->isClassCastable($key)) {
-            $this->setClassCastableAttribute($key, $value);
-
-            return $this;
-        }
-
-        if (! is_null($value) && $this->isJsonCastable($key)) {
-            $value = $this->castAttributeAsJson($key, $value);
-        }
-
-        // If this attribute contains a JSON ->, we'll set the proper value in the
-        // attribute's underlying array. This takes care of properly nesting an
-        // attribute in the array's value in the case of deeply nested items.
-        if (str_contains($key, '->')) {
-            return $this->fillJsonAttribute($key, $value);
-        }
-
-        if (! is_null($value) && $this->isEncryptedCastable($key)) {
-            $value = $this->castAttributeAsEncryptedString($key, $value);
-        }
-
-        if (! is_null($value) && $this->hasCast($key, 'hashed')) {
-            $value = $this->castAttributeAsHashedString($key, $value);
-        }
-
-        if ($this->isRelation($key)) {
+        if (($value instanceof Model || $value instanceof Collection) && $this->isRelation($key)) {
             return $this->setRelation($key, $value);
         }
 
-        $this->attributes[$key] = $value;
-
-        return $this;
+        return parent::setAttribute($key, $value);
     }
 }
